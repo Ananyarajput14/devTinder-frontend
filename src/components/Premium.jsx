afcb7523493f5e659fe2db8c4ace53561cb79e5e
Ananyarajput14 <ananyarajput652@gmail.com>
2025-07-30 20:31:26 +0530

    bugfix

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -63,7 +63,7 @@ const verifyPremiumUser = async() => {
           <svg className="h-5 w-5 text-green-500 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
           </svg>
-          Appear at top in search results
+          Chat with anyone on the platform
         </li>
         <li className="flex items-center">
           <svg className="h-5 w-5 text-green-500 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
